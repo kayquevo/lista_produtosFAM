6428d170e081e78c4a3eb5f2ecbb485263bc7732
Kayquevo <kayque5925@gmail.com>
2025-09-28 01:46:22 -0300

    versao do gustavo

--- application/Program.py
+++ application/Program.py
@@ -1,51 +1,104 @@
 # -*- coding: utf-8 -*-
 """
+
 @author: prof. Gregorio
 Disciplina: Estrutura de Dados
 """
+
 """
 Integrantes do Grupo:
-00351772 Kayque Viana Oliveira
+    RA  00352250    Gustavo Figueiredo Chapekausko:
+    RA  00351864    Gustavo Stabile
+    RA  00351772    Kayque Viana Oliveira
+    RA  00353170    Mariana Pirani da Silva
+    RA  00353171    Vinicius Joaquim da Silva
+
 """
 
-PRODUTOS = "application/lista_produtos.txt"
+PRODUTOS = "lista_produtos.txt"
+
 
 def carregar():
-    try:
-        print("\nCarregando os produtos do arquivo...\n")
-        inFile = open(PRODUTOS, 'r', encoding='utf-8')
+    """
+    Retorna uma string dos produtos e seus preços no formato:
+        produto1 preco1 produto2 preco2 ... produtoN precoN
+    Todas os produtos estão escrito em letras minúsculas.
+    O teste do programa será feito com um arquivo com dados
+        diferentes
+    """
+    print("\nCarregando os produtos do arquivo...\n")
 
-        dados = inFile.read()
+    # inFile: file
+    inFile = open(PRODUTOS, 'r')
 
-        dados_limpos = dados.lower().strip()
+    # dados: string
+    dados = inFile.readline()
+    inFile.close()
+    return dados
 
-        return dados_limpos
-    except FileNotFoundError:
-        print("Erro: arquivo não encontrado!")
-        return ""
-    
-prod = carregar()
+
+'''
+ As linhas seguintes são para testes, podem ser comentadas
+ para o programa final
+'''
+#prod = carregar()
+#print(prod)
 
 
 def lista_tupla(aLista):
+    '''
+    Parameters
+    ----------
+    aLista : lista com os produtos
 
-    lista_dos_produtos = aLista.split()
-    lista_de_pares = []
+    Returns
+    -------
+    Uma Tupla com os produtos no formato
+    ( (<str>produto1, <int>valor1), (<str>produto1, <int>valor1), ... )
+    '''
+    tupla = [] #Troquei "tupla" de tupla pra lista para usar o comando .append() pra adicionar valores mais facilmente na tupla
+    for i in range(0, len(aLista), 2):
+        produto = aLista[i]
+        preco = int(aLista[i+1])
+        tupla.append((produto, preco))
 
-    for i in range(0, len(lista_dos_produtos), 2):
-        produto = lista_dos_produtos[i]
-        preco = int(lista_dos_produtos[i+1])
-
-        lista_de_pares.append((produto, preco))
-        
-    tupla = tuple(lista_de_pares)
-    
-    return tupla
-
-tup = lista_tupla(prod)
-print(tup)
+    return tuple(tupla)#Transformar "tupla" em tupla
 
 
-        
-            
-    
+def get_Produtos(aTupla):
+    '''
+    Parameters
+    ----------
+    aTupla : Uma Tupla com os produtos no formato
+    ( (<str>produto1, <int>valor1), (<str>produto1, <int>valor1), ... )
+
+    Returns
+    -------
+    uma tupla com 3 informações abaixo:
+    1. O produto mais Barato (tupla): <produto> <valor>
+    2. O produto mais caro   (tupla): <produto> <valor>
+    3. Quantidade de Produtos Diferentes : <int>
+    '''
+    produto_mais_barato = aTupla[0]
+    produto_mais_caro = aTupla[0]
+    for produto in aTupla:
+        if produto[1] < produto_mais_barato[1]:
+            produto_mais_barato = produto
+        if produto[1] > produto_mais_caro[1]:
+            produto_mais_caro = produto
+
+    quantidade = len(aTupla)
+
+    return (produto_mais_barato, produto_mais_caro, quantidade)
+
+
+def main():
+    texto = carregar()
+    lista = texto.split()
+    dados = lista_tupla(lista)
+    resultado = get_Produtos(dados)
+    print("Produto mais barato: {}".format(resultado[0]))
+    print("Produto mais caro: {}".format(resultado[1]))
+    print("Quantidade de produtos: {}".format(resultado[2]))
+
+main()
